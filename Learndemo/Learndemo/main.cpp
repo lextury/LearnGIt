b8b84f50ef35232238749760a73fe633f32debed
lextury <3299755753@qq.com>
2024-06-28 15:48:45 +0800

    First Commit

---
+++ Learndemo/Learndemo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learndemo
+//
+//  Created by lextury on 2024/6/28.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
